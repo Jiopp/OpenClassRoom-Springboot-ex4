53a6013bf203407ff295cbae5d5aab1a8b4c05c0
Julien Deleligne <julien.deleligne@decathlon.com>
2021-02-17 11:56:08 +0100

    call api

--- src/main/java/com/openclassrooms/webapp/WebappApplication.java
+++ src/main/java/com/openclassrooms/webapp/WebappApplication.java
@@ -1,11 +1,15 @@
 package com.openclassrooms.webapp;
 
+import com.openclassrooms.webapp.repository.EmployeeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebappApplication {
+public class WebappApplication{
 
+	@Autowired
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
